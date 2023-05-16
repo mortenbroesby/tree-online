abf433f70f57a39e05bf3ca79b6988c63e002b4e
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2023-05-16 21:29:07 +0200

    Modify script

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Replaces a placeholder in the build output with a string value
+populate_placeholder() {
+  echo "Replacing all occurrences of '$1' with '$2'..."
+  grep -rl $1 build | xargs sed -i'.original' -e "s~$1~$2~g"
+}
+
 echo 'Injecting variables into the build output...'
 
-TIMESTAMP=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-SHORT_SHA=$(echo ${GITHUB_SHA} | cut -c1-7)
-PROJECT_URL="https://github.com/${GITHUB_REPOSITORY}"
-
-grep -rl '%%%GITLAB_CI_TIMESTAMP%%%' build | xargs sed -i'.original' -e "s~%%%GITLAB_CI_TIMESTAMP%%%~$TIMESTAMP~g"
-grep -rl '%%%CI_COMMIT_SHORT_SHA%%%' build | xargs sed -i'.original' -e "s~%%%CI_COMMIT_SHORT_SHA%%%~$SHORT_SHA~g"
-grep -rl '%%%CI_PROJECT_URL%%%' build | xargs sed -i'.original' -e "s~%%%CI_PROJECT_URL%%%~$PROJECT_URL~g"
+populate_placeholder '%%%GITLAB_CI_TIMESTAMP%%%' `date -u +'%Y-%m-%dT%H:%M:%SZ'`
+populate_placeholder '%%%CI_COMMIT_SHORT_SHA%%%' $(echo ${GITHUB_SHA} | cut -c1-7)
+populate_placeholder '%%%CI_PROJECT_URL%%%' "https://github.com/${GITHUB_REPOSITORY}"
