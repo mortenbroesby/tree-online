5c20ccf3e3d94a8a94da5c191a68fc184b8e6edc
mortenbroesby <mortenbroesby@users.noreply.github.com>
2023-05-17 15:30:30 +0000

    deploy: 62f7fcbfe750fdbcae59fb21c670ca2024501f1d

--- precache-manifest.76b212f3d8d01bec49583338cec40ad6.js
+++ precache-manifest.76b212f3d8d01bec49583338cec40ad6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b7dfbdfd00c9c5a8926140404535cbf",
+    "revision": "883e929863dc055f98f6be013f0ed6fa",
     "url": "/tree-online/index.html"
   },
   {
-    "revision": "a237564d024c75e09746",
+    "revision": "15d41fb0710aef177737",
     "url": "/tree-online/static/css/main.2c2ba62e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tree-online/static/js/2.1844f93e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a237564d024c75e09746",
-    "url": "/tree-online/static/js/main.bd2f1c68.chunk.js"
+    "revision": "15d41fb0710aef177737",
+    "url": "/tree-online/static/js/main.9e3bc940.chunk.js"
   },
   {
     "revision": "ff1797bb461899bec9af",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tree-online/precache-manifest.ad6c5dd62d74db47df1ca0d6548d299e.js"
+  "/tree-online/precache-manifest.76b212f3d8d01bec49583338cec40ad6.js"
 );
 
 self.addEventListener('message', (event) => {
